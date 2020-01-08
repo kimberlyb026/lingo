6bc8e9b4d03eba2efd676749d893dc15a8e8b8a7
kimberly <99051407@mydavinci.nl>
2020-01-08 11:11:06 +0100

    4

--- lingo-nl.js
+++ lingo-nl.js
@@ -478,3 +478,132 @@ var words = [
 	"zeker",
 	"zever",
 	"zeeen"];
+
+var input = document.getElementById('guess'); // the input box
+var button = document.getElementById('button'); // the button
+var guess;
+
+// change css class
+var changeClass = function(cng, old, newClass){
+  cng.className = cng.className.replace(old, newClass);
+}
+
+// game loop
+var gameloop = function(){
+  // pick a random word
+  var rand = words[Math.floor(Math.random() * words.length)];
+  var hasDuplicates = (/([a-zA-Z]).*?\1/).test(rand); // if multiple insances of a letter in the word
+  
+  var pressn = 1; // turn number
+  
+  // get all indexes of a given value in an array
+  var getAllIndexes = function(arr, val) {
+    var indexes = [], i;
+    for(i = 0; i < arr.length; i++)
+        if (arr[i] === val)
+            indexes.push(i);
+    return indexes;
+  }
+  
+  // give first letter
+  document.getElementById("row1").firstElementChild.innerHTML=rand[0];
+  
+  // guess event
+  input.onkeypress = function(event) {
+    if (event.key == "Enter" || event.keyCode == 13) {
+      document.getElementById('smallMsg').innerHTML = "Green = correct letter, Yellow = wrong place"; // reset message
+      guess = input.value.toUpperCase();
+      
+      var current = "row" + pressn;
+      // current row
+      var childDivs = document.getElementById(current).getElementsByTagName('div');
+      var c = 0; // correct count
+      
+      // If not right number of letters
+      if(guess.length !== 5){
+        document.getElementById('smallMsg').innerHTML = "Guesses must be 5 letters!";
+        if(pressn===5){
+          end("", "goede woord: " + rand);
+        }
+        pressn++;
+        document.getElementById(current).firstElementChild.innerHTML=rand[0];
+        return; // move down
+      }
+
+      // check for correctness
+      for(var i=0; i<childDivs.length; i++) {
+        childDivs[i].innerHTML = guess[i];
+        
+        // if letter match in right place
+        if(guess[i] == rand[i]){
+          changeClass(childDivs[i], 'default', 'correct');
+          c++;
+        } 
+        // if exists but is in the wrong place
+        else if(rand.indexOf(guess[i])!=-1){
+          if(hasDuplicates === false && childDivs[rand.indexOf(guess[i])].className != "square correct"){
+            changeClass(childDivs[i], 'default', 'wrongplace');
+          } //if
+          // if there are duplicate letters
+          else if(hasDuplicates === true){
+            var ind = getAllIndexes(rand, guess[i]);
+            if (ind.length > 1){
+              for (var j=0; j<ind.length;j++){
+                if(childDivs[ind[j]].className != "square correct" && childDivs[i].className != "square wrongplace"){
+                  changeClass(childDivs[i], 'default', 'wrongplace',);
+                } //if
+              } //for
+            } //if
+            else if (childDivs[rand.indexOf(guess[i])].className != "square correct"){
+              changeClass(childDivs[i], 'default', 'wrongplace');
+            } //else if
+          } //else if(hasDuplicates === true)
+        } //else if
+        
+        input.value = ""; // clear input box
+        
+        if(c===5) { // if they have all the correct letters
+          end("je hebt het woord geraden");
+        } //if
+        else if (pressn === 5){ // if they're out of tries
+          end(".", "goede woord: " + rand);
+        } //else if
+      } //for (check for correctness loop)
+      pressn++; // inc number of guesses
+    } //if (key = 'enter')
+  } //input 
+} //gameloop
+
+// endgame
+var end = function(msg, smallmsg){
+  document.getElementById('msgBox').innerHTML = msg;
+  document.getElementById('smallMsg').innerHTML = smallmsg;
+  changeClass(button, "visible", "visible");
+  document.getElementById('guess').readOnly = true;
+}
+
+// reset
+var playagain = function(){
+  document.getElementById('msgBox').innerHTML="Guess the Word!"; // main message
+  document.getElementById('smallMsg').innerHTML = 
+  document.getElementById('guess').readOnly = false;
+  changeClass(button, "visible", "visible");
+  
+  // clean boxes
+  for(var i=1;i<6;i++){
+    var resets = document.getElementById('row'+i).getElementsByTagName('div');
+    for(var j=0;j<5;j++){
+      resets[j].innerHTML="";
+      if(resets[j].className == "square correct" || resets[j].className == "square wrongplace"){
+        changeClass(resets[j], "correct", "default");
+        changeClass(resets[j], "wrongplace", "default");
+      } //if
+    } //for
+  } //for
+  // restart the loop
+  gameloop();
+};
+
+// start loop
+gameloop();
+
